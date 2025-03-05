44a0704b010e0e1ed6a4f7bec423e61bb5b3d957
piyush goyal <piygoyal30@gmail.com>
2025-03-05 13:34:55 +0530

    [Piyush Goyal] add custom exception create and throw exception

--- src/main/java/com/example/EmployeePayrollApp/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.EmployeePayrollApp.repository;
-
-import com.example.EmployeePayrollApp.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
